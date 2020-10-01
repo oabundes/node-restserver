9c22973f618ae490333c2bba423da290a01a98ee
[OAG] <[oabundesg@gmail.com]>
2020-09-30 19:24:17 -0500

    Cambio en caducidad de token

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // 60 min
 // 24 hrs
 // 30 dias
-process.env.CADUCIDAD_TOKEN = 60 * 60 //* 24 * 30;
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
 
 
